152d11fb4eda77d720ed3b03767ec3b09dcb5620
Encoded <75113441+Encoded404@users.noreply.github.com>
2025-03-06 13:57:13 +0100

    give variable a more representetive name

--- sockets/socketHelper.cs
+++ sockets/socketHelper.cs
@@ -2,7 +2,7 @@ namespace WireLink
 {
     internal static class SocketHelper
     {
-        public static int ExtraMessageLength = PrepareMessage([]).Length + 1; // add 1 to be safe
+        public static int MessagePaddingLength = PrepareMessage([]).Length + 1; // add 1 to be safe
 
         public static byte[] PrepareMessage(byte[] data)
         {
